cbf4e2385464428477197fde77551eea466362f5
RaistlinD <Johnd0712@gmail.com>
2018-10-29 21:20:38 +0800

    ok

---
+++ pages/posts/post-detail/post-detail.js
@@ -0,0 +1,83 @@
+var postsData = require("../../../data/posts-data.js")
+
+Page({
+  onLoad: function(option) {
+    var postId = option.id;
+    // console.log(postId);
+    var postData = postsData.postList[postId];
+    this.setData({
+      postData: postData,
+      postId: postId
+    });
+
+    var postsCollected = wx.getStorageSync('posts_collected');
+    if (postsCollected) {
+      var postCollected = postsCollected[postId];
+      if (postCollected == null) {
+        postCollected = false;
+      }
+
+      this.setData({
+        collected: postCollected
+      })
+    } else {
+      var postsCollected = {};
+      postsCollected[postId] = false;
+      wx.setStorageSync('posts_collected', postsCollected);
+      this.setData({
+        collected: false
+      })
+    }
+  },
+
+  onCollectionTap: function(event) {
+    var postsCollected = wx.getStorageSync('posts_collected');
+    var postCollected = postsCollected[this.data.postId];
+    postCollected = !postCollected;
+    // console.log(postCollected);
+    postsCollected[this.data.postId] = postCollected;
+
+    this.showToast(postsCollected, postCollected);
+
+    // console.log(this.data.collected);
+  },
+
+  showModal: function(postsCollected, postCollected) {
+    var that = this;
+    wx.showModal({
+      title: '收藏',
+      content: postCollected ? '是否收藏该文章?' : '取消收藏该文章?',
+      showCancel: 'true',
+      cancelText: '取消',
+      cancelColor: '#333',
+      confirmText: '确认',
+      confirmColor: '#405f80',
+      success: function(res) {
+        if (res.confirm) {
+          wx.setStorageSync('posts_collected', postsCollected);
+          that.setData({
+            collected: postCollected
+          })
+        }
+      }
+    })
+  },
+
+  showToast: function(postsCollected, postCollected) {
+    wx.setStorageSync('posts_collected', postsCollected);
+
+    this.setData({
+      collected: postCollected
+    })
+
+    wx.showToast({
+      title: postCollected ? '收藏成功' : '取消成功',
+      duration: 1000
+    })
+  },
+
+  onShareTap: function(event) {
+
+  }
+
+})
