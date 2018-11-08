dbb28baca854fce547ff99889a17743b796b510d
RaistlinD <Johnd0712@gmail.com>
2018-11-08 20:53:28 +0800

    ok

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -1,19 +1,31 @@
+var app = getApp();
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
+    var inTheatersUrl = app.globalData.doubanBase + "/v2/movie/in_theaters";
+    var comingSoonUrl = app.globalData.doubanBase + "/v2/movie/coming_soon";
+    var top250Url = app.globalData.doubanBase + "/v2/movie/top250";
+
+    this.getMovieListData(inTheatersUrl);
+    this.getMovieListData(comingSoonUrl);
+    this.getMovieListData(top250Url);
+
+  },
+
+  getMovieListData: function(url) {
     wx.request({
-      url: 'http://t.yushu.im/v2/movie/top250',
-      data: {},
+      url: url,
       method: 'GET',
       header: {
         "content-type": "application/json",
@@ -28,54 +40,7 @@ Page({
 
       }
     })
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
   }
+
+
 })
