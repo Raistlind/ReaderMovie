773328fbdb3316c04b0a338d952d287a58a34d8e
RaistlinD <Johnd0712@gmail.com>
2018-11-11 23:15:43 +0800

    ok

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -1,3 +1,4 @@
+var util = require('../../utils/util.js');
 var app = getApp();
 
 Page({
@@ -6,7 +7,9 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    inTheaters: {},
+    comingSoon: {},
+    top250: {}
   },
 
   /**
@@ -17,13 +20,13 @@ Page({
     var comingSoonUrl = app.globalData.doubanBase + "/v2/movie/coming_soon" + "?start=0&count=3";
     var top250Url = app.globalData.doubanBase + "/v2/movie/top250" + "?start=0&count=3";
 
-    this.getMovieListData(inTheatersUrl);
-    // this.getMovieListData(comingSoonUrl);
-    // this.getMovieListData(top250Url);
-//
+    this.getMovieListData(inTheatersUrl, "inTheaters");
+    this.getMovieListData(comingSoonUrl, "comingSoon");
+    this.getMovieListData(top250Url, "top250");
+
   },
 
-  getMovieListData: function(url) {
+  getMovieListData: function(url, settedKey) {
     var that = this;
     wx.request({
       url: url,
@@ -33,7 +36,7 @@ Page({
       },
       success: function(res) {
         console.log(res);
-        that.processDoubanData(res.data);
+        that.processDoubanData(res.data, settedKey);
       },
       fail: function() {
         console.log("failed");
@@ -44,15 +47,16 @@ Page({
     })
   },
 
-  processDoubanData: function(moviesDouban) {
+  processDoubanData: function(moviesDouban, settedKey) {
     var movies = [];
-    for(var idx in moviesDouban.subjects) {
+    for (var idx in moviesDouban.subjects) {
       var subject = moviesDouban.subjects[idx];
       var title = subject.title;
       if (title.length >= 6) {
-        title = title.substring(0,6) + "...";
+        title = title.substring(0, 6) + "...";
       }
       var temp = {
+        stars: util.convertToStarsArray(subject.rating.stars),
         title: title,
         average: subject.rating.average,
         coverageUrl: subject.images.large,
@@ -60,9 +64,11 @@ Page({
       }
       movies.push(temp);
     }
-    this.setData({
+    var readyData = {};
+    readyData[settedKey] = {
       movies: movies
-    })
+    };
+    this.setData(readyData);
   }
 
 
